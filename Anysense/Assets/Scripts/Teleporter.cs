b93f08123676e840d6f3ffbca267c4eeb0cdcdb9
ikke123123 <45997197+ikke123123@users.noreply.github.com>
2020-04-10 10:45:17 +0200

    Updated Color Spectrum & Added Scripts

---
+++ Anysense/Assets/Scripts/Teleporter.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TimeStart { stop, start };
+
+public class Teleporter : MonoBehaviour
+{
+    [SerializeField] private Locations[] locations;
+    [SerializeField] private SimpleSoundCardPlayer soundcardPlayer;
+    [SerializeField] private HappinessMeter happinessMeter;
+
+    private void Awake()
+    {
+        if (locations.Length == 0)
+        {
+            Debug.LogError("Add locations.");
+            return;
+        }
+        gameObject.transform.position = locations[0].position.position;
+        gameObject.transform.rotation = locations[0].position.rotation;
+        soundcardPlayer.StartPlaying(locations[0].soundcard);
+    }
+
+    public void GoToLocation(int location)
+    {
+        if (location >= locations.Length) return;
+        if (locations[location].timeStart == TimeStart.start)happinessMeter.StartCounting();
+        else happinessMeter.StopCounting();
+        soundcardPlayer.StartPlaying(locations[location].soundcard);
+        transform.position = locations[location].position.position;
+        transform.rotation = locations[location].position.rotation;
+    }
+}
+
+[System.Serializable]
+public class Locations
+{ 
+    [SerializeField] public string name;
+    [SerializeField] public Transform position;
+    [SerializeField] public Soundcard soundcard;
+    [SerializeField] public TimeStart timeStart;
+}
