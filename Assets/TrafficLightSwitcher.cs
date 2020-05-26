dee5fd4651da523b7ae286363106b35c521c7587
ikke123123 <thamasbrouwer1001@gmail.com>
2020-05-26 11:53:17 +0200

    Light Switcher Addition

---
+++ Assets/TrafficLightSwitcher.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficLightSwitcher : MonoBehaviour
+{
+    [SerializeField] private MeshRenderer meshRenderer;
+    [SerializeField] private Color goColor;
+    [SerializeField] private Color stopColor;
+
+    [SerializeField] private bool active;
+
+    private bool isActive;
+
+    private void Start()
+    {
+        active = isActive;
+        SetGo(isActive);
+    }
+
+    private void Update()
+    {
+        SetGo(active);
+    }
+
+    public bool SetGo(bool active)
+    {
+        if (active != isActive)
+        {
+            isActive = active;
+            if (isActive == true)
+            {
+                SetColor(goColor);
+            }
+            else SetColor(stopColor);
+        }
+        return isActive;
+    }
+
+    private void SetColor(Color color)
+    {
+        meshRenderer.material.SetColor("_Color", color);
+        
+        meshRenderer.material.SetColor("_EmissionColor", color);
+    }
+}
