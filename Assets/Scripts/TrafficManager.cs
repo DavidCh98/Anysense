1df94a745c5fdd493e6cce809b9fd4713fcf93a1
ikke123123 <thamasbrouwer1001@gmail.com>
2020-06-10 01:34:46 +0200

    Not a BruhCommit

--- Assets/Scripts/TrafficManager.cs
+++ Assets/Scripts/TrafficManager.cs
@@ -16,83 +16,61 @@ public class TrafficManager : MonoBehaviour
     [SerializeField] private Material redScreen = null;
 
     [Header("Setup")]
-    [SerializeField] private Color goColor = Color.red;
-    [SerializeField] private Color stopColor = Color.green;
+    [SerializeField] private Color red = Color.red;
+    [SerializeField] private Color green = Color.green;
     [SerializeField, Range(0.001f, 5)] private float transitionLength = 0.001f;
     [SerializeField, Range(1, 5)] private float emission = 4;
 
     [Header("Timer")]
-    [SerializeField] private bool timerEnable = true;
-    [SerializeField, Range(5, 20)] private int goTimer = 0;
-    [SerializeField, Range(5, 50)] private int stopTimer = 0;
+    [SerializeField, Range(5, 50)] private int carGoTimer = 5;
+    [SerializeField, Range(5, 20)] private int carStopTimer = 5;
 
     [Header("Debug")]
-    [SerializeField] private bool pedestrianGo;
-
-    public bool greenLight = false;
+    public bool greenPedestrianLight = false;
     public bool transitioning = false;
-
-    private bool go = true;
-    private float transition = 0;
-    private Color targetColor = Color.white;
-    private Color currentColor = Color.white;
-    private float timer = 0;
-    private bool transitionIgnore = false;
+    public bool active = true;
 
     private void Start()
     {
-        SwitchPedestrianColor();
+        StartCoroutine(Loop());
     }
 
-    private void Update()
+    IEnumerator Loop()
     {
-        //Debug
-        if (pedestrianGo)
+        while (active)
         {
-            SwitchPedestrianColor();
-            pedestrianGo = CodeLibrary.FlipBool(pedestrianGo);
-        }
-        //\Debug
-
-        //Timer
-        if (timer < 0)
-        {
-            SwitchPedestrianColor();
-            timer = 0;
-        }
-        else if (timer > 0) timer -= Time.deltaTime;
-
-        //Transitions
-        if (transition > transitionLength)
-        {
-            SetPedestrianColor(targetColor);
-            SetCarColor(go ? CarColor.green : CarColor.red);
-            transition = transitionLength;
-            if (timerEnable) timer = go ? goTimer : stopTimer;
-            greenLight = go;
             transitioning = false;
+            greenPedestrianLight = false;
+            SetCarColor(CarColor.green);
+            SetPedestrianColor(red);
+            yield return new WaitForSeconds(carGoTimer);
+            SetCarColor(CarColor.orange);
+            transitioning = true;
+            yield return new WaitForSeconds(transitionLength);
+            transitioning = false;
+            greenPedestrianLight = true;
+            SetCarColor(CarColor.red);
+            SetPedestrianColor(green);
+            yield return new WaitForSeconds(carStopTimer - transitionLength);
+            StartCoroutine(PedestrianTransition());
+            transitioning = true;
+            yield return new WaitForSeconds(transitionLength);
         }
-        else if (transition < transitionLength)
+    }
+
+    IEnumerator PedestrianTransition()
+    {
+        for (float i = 0; transitionLength > i; i += Time.deltaTime)
         {
-            transition += Time.deltaTime;
-            CalculatePedestrianColor();
+            SetPedestrianColor(Color.Lerp(green, red, CodeLibrary.Remap(i, 0, transitionLength, 0, 1)));
+            yield return new WaitForFixedUpdate();
         }
     }
 
-    public void SwitchPedestrianColor()
+    private void SetCarColor(CarColor color)
     {
-        targetColor = go ? stopColor : goColor;
-        transition = 0;
-        transitioning = true;
-        greenLight = false;
-        SetCarColor(CarColor.orange);
-        go = CodeLibrary.FlipBool(go);
-        currentColor = pedestrianTrafficMesh[0].material.color;
-    }
-
-    private void CalculatePedestrianColor()
-    {
-        SetPedestrianColor(Color.Lerp(currentColor, targetColor, CodeLibrary.Remap(transition, 0, transitionLength, 0, 1)));
+        Material material = color == CarColor.green ? greenScreen : (color == CarColor.orange ? orangeScreen : redScreen);
+        foreach (MeshRenderer carScreen in carTrafficScreen) carScreen.material = material;
     }
 
     private void SetPedestrianColor(Color color)
@@ -102,11 +80,5 @@ public class TrafficManager : MonoBehaviour
             pedestrianMesh.material.SetColor("_Color", color);
             pedestrianMesh.material.SetColor("_EmissionColor", color * emission);
         }
-    }
-
-    private void SetCarColor(CarColor color)
-    {
-        Material material = color == CarColor.green ? greenScreen : (color == CarColor.orange ? orangeScreen : redScreen);
-        foreach (MeshRenderer carScreen in carTrafficScreen) carScreen.material = material;
     }
 }
