5fc65e750049d16a36097969b8426edcca9896e0
DavidCh98 <47038868+DavidCh98@users.noreply.github.com>
2020-06-13 18:16:10 +0200

    Some sounds added

---
+++ Assets/FootSteps.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootSteps : MonoBehaviour
+{
+
+	public GameObject objectWithScript;
+	FMOD.Studio.EventInstance Movement;
+	private bool moveForward;
+
+	// Use this for initialization
+	void Start()
+	{
+		Movement = FMODUnity.RuntimeManager.CreateInstance("event:/PlayerMovement");
+		moveForward = objectWithScript.GetComponent<OVRPlayerController>();
+	}
+
+		// Update is called once per frame
+		void FixedUpdate()
+		{
+				if (moveForward)
+				{
+					Movement.start();
+				}
+				else
+				{
+					Movement.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+				}
+		}
+		private void OnDestroy()
+		{
+		Movement.release();
+		Movement.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+		}
+}
